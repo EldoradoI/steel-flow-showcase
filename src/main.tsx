8401f84b0524c37de60b6520ffa34f277a363c99
EldoradoI <evan.engels1995@gmail.com>
2025-09-13 14:43:54 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
+// 👇 У деві ('/') для Lovable, у проді ('/steel-flow-showcase') для GitHub Pages
+const baseName = import.meta.env.PROD ? "/steel-flow-showcase" : "/";
+
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/steel-flow-showcase">
+  <BrowserRouter basename={baseName}>
     <App />
   </BrowserRouter>
 );
